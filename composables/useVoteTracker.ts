d9afd6b819e1fc9f28b09a72483a9951591133ad
rakeem <rakeemxng@gmail.com>
2025-07-13 08:48:10 -0400

    fixed double voting clause if session is truthy

--- composables/useVoteTracker.ts
+++ composables/useVoteTracker.ts
@@ -1,29 +1,29 @@
 const VOTE_STORAGE_KEY = "votedPolls";
 
 export const useVoteTracker = (pollId: number) => {
-  const hasVoted = computed(() => {
-    if (typeof localStorage === "undefined") return false;
+  const votedRef = ref(false);
 
+  onMounted(() => {
     const votedPolls = JSON.parse(
       localStorage.getItem(VOTE_STORAGE_KEY) || "[]"
     );
-    return votedPolls.includes(pollId);
+    votedRef.value = votedPolls.includes(pollId);
   });
 
   function markVoted() {
-    if (typeof localStorage === "undefined") return;
-
     const votedPolls = JSON.parse(
       localStorage.getItem(VOTE_STORAGE_KEY) || "[]"
     );
+
     if (!votedPolls.includes(pollId)) {
       votedPolls.push(pollId);
       localStorage.setItem(VOTE_STORAGE_KEY, JSON.stringify(votedPolls));
+      votedRef.value = true;
     }
   }
 
   return {
-    hasVoted,
+    hasVoted: votedRef,
     markVoted,
   };
 };
