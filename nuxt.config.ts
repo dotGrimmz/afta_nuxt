727f672e81033d6c1a4013a50b89f20e17dcd161
rakeem <rakeemxng@gmail.com>
2025-07-19 13:15:16 -0400

    .env.local

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  runtimeConfig: {
+    public: {
+      supabaseUrl: process.env.SUPABASE_URL,
+      supabaseAnonKey: process.env.SUPABASE_KEY,
+    },
+  },
   compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
   modules: ["@nuxtjs/supabase", "@nuxtjs/tailwindcss", "@nuxt/ui"],
