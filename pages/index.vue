9d83befae5bc922a58c8c918d63128b4f7f55d8e
rakeem <rakeemxng@gmail.com>
2025-07-29 22:32:42 -0400

    ok its named AFTA.. dont ask

--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,27 @@
 <template>
   <div>
-    <!-- <ParallaxHero
-      imageUrl="/images/grimmz.jpg"
-      height="h-[30vh] md:h-[50vh]"
-      :darken="false"
-    >
-      <div>
-        <h1 class="text-4xl md:text-6xl font-extrabold mb-4">
-          Welcome to AFTA
-        </h1>
-        <p class="text-lg md:text-2xl text-white/80 mb-6">
-          Another Failed Tagged App, but bold.
-        </p>
-        <BaseButton size="lg"
-          >⚠️Keep in mind this is not PRODUCTION READY⚠️</BaseButton
-        >
-      </div>
-    </ParallaxHero> -->
-
-    <Hyperspeed class="relative h-screen w-full">
-      <div class="absolute inset-0 flex items-center justify-center text-white">
-        <h1 class="text-5xl font-bold">Your Title Here</h1>
-      </div>
-    </Hyperspeed>
-    <!-- <section height="h-[30vh] md:h-[50vh]">
+    <section class="relative w-full h-[30vh]">
       <Hyperspeed />
-    </section> -->
+      <div
+        class="top-0 left-0 z-1 absolute flex flex-col justify-center items-center w-full h-full pointer-events-none"
+      >
+        <p
+          class="mt-4 max-w-[18ch] font-bold text-[clamp(2rem,4vw,2.6rem)] text-white text-center leading-[1.2] tracking-[-2px]"
+          :style="{
+            backgroundImage:
+              'linear-gradient(135deg, #0f0f1f, #331d2c, #6f1a07)',
+
+            WebkitBackgroundClip: 'text',
+            WebkitTextFillColor: 'transparent',
+            textShadow: '0 0 16px rgba(0, 0, 0, 0.5)',
+            filter: 'drop-shadow(0 0 6px #00ffff)',
+          }"
+        >
+          AFTA
+        </p>
+      </div>
+    </section>
+
     <div class="flex flex-col gap-4 bg-black p-4 min-h-screen">
       <PollsPollSection />
     </div>
